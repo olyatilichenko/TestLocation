68b2f7d6081997c95219ffd54c2ec7fb21b7470f
Olya <olyat96@gmail.com>
2018-02-27 15:37:27 +0300

    Getting information about the current location

--- TestLocation/ViewController.swift
+++ TestLocation/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ViewController: UIViewController {
-
+    
+    var placesClient: GMSPlacesClient!
+    private let locationManager = CLLocationManager()
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        placesClient = GMSPlacesClient.shared()
+        locationManager.requestWhenInUseAuthorization()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
+        
+        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                        .joined(separator: "\n")
+                }
+            }
+        })
     }
-
-
 }
-
